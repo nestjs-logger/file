73e759f346f3c8e52da78ac2cd24141e0f99ce70
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2022-12-22 22:18:57 +0800

    FIX: stuck ups

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import type { LogLevel, LoggerService } from '@nestjs/common';
-import { appendFileSync } from 'fs';
+import { WriteStream, createWriteStream, existsSync, writeFileSync } from 'fs';
 import { join } from 'path';
 import { EOL } from 'os';
 import dayjs from 'dayjs';
@@ -19,7 +19,13 @@ export type Options = {
 	extension?: string;
 };
 
+type Streams = {
+	[key: string]: WriteStream;
+};
+
 export class FileLogger implements LoggerService {
+	protected streams: Streams = {};
+
 	constructor(protected options: Options) {
 		//
 	}
@@ -28,6 +34,18 @@ export class FileLogger implements LoggerService {
 		const { directory, filename, extension } = this.options;
 
 		return join(directory, `${filename ?? 'nest'}-${level}.${extension ?? 'log'}`);
+	}
+
+	protected write(fileName: string, data: string) {
+		if (!existsSync(fileName)) {
+			writeFileSync(fileName, '');
+		}
+
+		if (!(fileName in this.streams)) {
+			this.streams[fileName] = createWriteStream(fileName, { flags: 'a', autoClose: true });
+		}
+
+		this.streams[fileName].write(data);
 	}
 
 	log(message: any, ...optionalParams: any[]) {
@@ -40,7 +58,7 @@ export class FileLogger implements LoggerService {
 			'\t'
 		);
 
-		appendFileSync(this.createPath('log'), `[${dayjs().format('YYYY-MM-DD HH:mm:ss')}] ${data}${EOL}`);
+		this.write(this.createPath('log'), `[${dayjs().format('YYYY-MM-DD HH:mm:ss')}] ${data}${EOL}`);
 	}
 
 	error(message: any, ...optionalParams: any[]) {
@@ -53,7 +71,7 @@ export class FileLogger implements LoggerService {
 			'\t'
 		);
 
-		appendFileSync(this.createPath('error'), `[${dayjs().format('YYYY-MM-DD HH:mm:ss')}] ${data}${EOL}`);
+		this.write(this.createPath('error'), `[${dayjs().format('YYYY-MM-DD HH:mm:ss')}] ${data}${EOL}`);
 	}
 
 	warn(message: any, ...optionalParams: any[]) {
@@ -66,7 +84,7 @@ export class FileLogger implements LoggerService {
 			'\t'
 		);
 
-		appendFileSync(this.createPath('warn'), `[${dayjs().format('YYYY-MM-DD HH:mm:ss')}] ${data}${EOL}`);
+		this.write(this.createPath('warn'), `[${dayjs().format('YYYY-MM-DD HH:mm:ss')}] ${data}${EOL}`);
 	}
 
 	debug(message: any, ...optionalParams: any[]) {
@@ -79,7 +97,7 @@ export class FileLogger implements LoggerService {
 			'\t'
 		);
 
-		appendFileSync(this.createPath('debug'), `[${dayjs().format('YYYY-MM-DD HH:mm:ss')}] ${data}${EOL}`);
+		this.write(this.createPath('debug'), `[${dayjs().format('YYYY-MM-DD HH:mm:ss')}] ${data}${EOL}`);
 	}
 
 	verbose(message: any, ...optionalParams: any[]) {
@@ -92,7 +110,7 @@ export class FileLogger implements LoggerService {
 			'\t'
 		);
 
-		appendFileSync(this.createPath('verbose'), `[${dayjs().format('YYYY-MM-DD HH:mm:ss')}] ${data}${EOL}`);
+		this.write(this.createPath('verbose'), `[${dayjs().format('YYYY-MM-DD HH:mm:ss')}] ${data}${EOL}`);
 	}
 
 	static create(options: Options) {
